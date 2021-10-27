b400d5184a7be161abf6c9e45422e9602d83e66d
Rodless <Trustful69@gmail.com>
2021-10-27 18:40:56 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,8 @@ import Footer from '@components/Footer'
 import Vis from '@components/Vis'
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="LOGIN!" />
-        <p className="description">
-          <p>
-            <input type="text" name="name" placeholder="Username" required/>
-          </p>
-          <p>
-            <input type="password" name="pass" placeholder="Password" required/>
-          </p>
-         </p>
-        <input type="submit" name="sub" value="Save"/>
-      </main>
-      <Vis />
-    </div>
+   
+       <Vis />
+  
   )
 }
