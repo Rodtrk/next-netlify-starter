e6e501bd3b206d479de8d07b50a718b22fcaf524
Rodless <Trustful69@gmail.com>
2021-10-27 13:35:46 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
 import styles from './Footer.module.css'
+import js from './Vis.js'
 
 export default function Footer() {
   return (
@@ -6,7 +7,7 @@ export default function Footer() {
       <footer className={styles}>
   <!-- style.footer-->
         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-        <a href="{Vis.js}" >click </a>
+        <a href={js} >click </a>
       </footer>
     </>
   )
