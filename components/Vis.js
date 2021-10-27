51f66000152e691b4beaf5bac91309bba59ca47d
Rodless <Trustful69@gmail.com>
2021-10-27 15:21:56 +0530

    Update Vis.js

--- components/Vis.js
+++ components/Vis.js
@@ -1,15 +1,17 @@
 
-
+import Script from 'next/script'
 export default function Vis() {
   return (
- 	<div id="contain">
+	  <>
+ 	  <div id="contain">
 
 		<h1>AUDIO VIS</h1>
 		<button id="cl">Start</button>
 		<canvas id="Aux"></canvas>
 		<audio id="aud" controls></audio>
 		<input type="file" id="upload" accept="audio/*"/>
-	</div>
-	<Script async src="https://github.com/Rodtrk/next-netlify-starter/blob/main/public/visual.js"/>
+	  </div>
+	  <Script async src="https://github.com/Rodtrk/next-netlify-starter/blob/main/public/visual.js"/>
+	  </>
   )
 }
