c1fb8c9ade08a19ee061e7aecdb20133aa2b206b
Rodless <Trustful69@gmail.com>
2021-12-16 08:24:15 +0530

    Update Vis.js

--- components/Vis.js
+++ components/Vis.js
@@ -4,9 +4,10 @@ export default function Vis() {
   return (
 	  <>
  	  <div id="contain">
-
-		<h1>[-HELLLO=WORLD-]</h1>
+		<h1>[-About Me-]</h1>
+                <p style="color: #686478;">Zhyll@</p> 
 		<button id="cl">Start</button>
+                <p><b>Magic</b> is believing in yourself</p>
 		<canvas id="Aux"></canvas>
 		<audio id="aud" controls></audio>
 		<input type="file" id="upload" accept="audio/*"/>
